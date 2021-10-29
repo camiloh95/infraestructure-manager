d18f8df01d6022598cd83ef63f9773cc657f0dd2
Camilo Hernandez <chernandez@lean-tech.io>
2021-10-29 12:29:56 -0500

    feat: - creating repositories with bitbucket API - handling error as await async more readable

--- src/providers/bitbucket/bitbucket.provider.ts
+++ src/providers/bitbucket/bitbucket.provider.ts
@@ -4,80 +4,73 @@ import config from '@config';
 
 export class BitbucketProvider {
   public async getAuthToken(): Promise<any> {
-    return new Promise((resolve, reject) => {
-      axios({
-          method: 'post',
-          baseURL: 'https://bitbucket.org/',
-          url: '/site/oauth2/access_token',
-          auth: {
-            username: config.bitbucket.clientId,
-            password: config.bitbucket.clientSecret
-          },
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          },
-          data: qs.stringify({
-            grant_type: 'client_credentials',
-          }),
-        })
-        .then((res:any) => {
-          resolve(res.data);
-        })
-        .catch((err:any) => {
-          reject(err);
-        });
-    });
+    try {
+      const response = await axios({
+        method: 'post',
+        baseURL: 'https://bitbucket.org/',
+        url: '/site/oauth2/access_token',
+        auth: {
+          username: config.bitbucket.clientId,
+          password: config.bitbucket.clientSecret
+        },
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data: qs.stringify({
+          grant_type: 'client_credentials',
+        }),
+      });
+
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
   }
 
   public async createProject(token: string, projectParams: any): Promise<any> {
-    return new Promise((resolve, reject) => {
-      axios({
-          method: 'post',
-          url: `https://api.bitbucket.org/2.0/workspaces/${config.bitbucket.worspace}/projects/`,
-          headers: {
-            'Authorization': 'Bearer ' + token,
-            'Content-Type': 'application/json'
-          },
-          data: {
-            key: projectParams.key,
-            name: projectParams.name,
-            description: projectParams.description
-          }
-        })
-        .then((res:any) => {
-          resolve(res.data);
-        })
-        .catch((err:any) => {
-          reject(err);
-        });
-    });
+    try {
+      const response = await axios({
+        method: 'post',
+        url: `https://api.bitbucket.org/2.0/workspaces/${config.bitbucket.worspace}/projects/`,
+        headers: {
+          'Authorization': 'Bearer ' + token,
+          'Content-Type': 'application/json'
+        },
+        data: {
+          key: projectParams.key,
+          name: projectParams.name,
+          description: projectParams.description
+        }
+      })
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
   }
 
-  public async createRepository(token: string, repositoryParams: any, projectKey: string): Promise<any> {
-    return new Promise((resolve, reject) => {
-      axios({
-          method: 'post',
-          url: `https://api.bitbucket.org/2.0/repositories/${config.bitbucket.worspace}`,
-          headers: {
-            'Authorization': 'Bearer ' + token,
-            'Content-Type': 'application/json'
-          },
-          data: {
-            name: repositoryParams.name,
-            is_private: true,
-            description: repositoryParams.description,
-            language: repositoryParams.language,
-            project: {
-              key: projectKey
-            }
+  public async createRepository(token: string, params: any): Promise<any> {
+    try {
+      const repoSlug = params.repository.name.toLowerCase().replace(/ /g, '-');
+      const response = await axios({
+        method: 'post',
+        url: `https://api.bitbucket.org/2.0/repositories/${config.bitbucket.worspace}/${repoSlug}`,
+        headers: {
+          'Authorization': 'Bearer ' + token,
+          'Content-Type': 'application/json'
+        },
+        data: {
+          name: params.repository.name,
+          is_private: true,
+          description: params.repository.description,
+          language: params.repository.language,
+          project: {
+            key: params.project.key
           }
-        })
-        .then((res:any) => {
-          resolve(res.data);
-        })
-        .catch((err:any) => {
-          reject(err);
-        });
-    });
+        }
+      });
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
   }
 }
